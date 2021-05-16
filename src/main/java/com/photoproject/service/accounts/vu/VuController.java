203ab16eca7575c1a68db0a2b4fed0254b6ed0ba
lyhuynhvu <LyVu@DESKTOP-MUI1CET.mshome.net>
2021-05-16 18:26:29 +0700

    just add Vu Controller

--- src/main/java/com/photoproject/service/accounts/vu/VuController.java
+++ src/main/java/com/photoproject/service/accounts/vu/VuController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.photoproject.service.accounts.greeting.Greeting;
+//import com.photoproject.service.accounts.greeting.Greeting;
 
 import java.util.concurrent.atomic.AtomicLong;
 
