fd94c93e861ddde58b3a631bd90a50c7458fe9af
Del <del@babel.com.au>
2021-05-25 13:19:07 +0700

    Change Nghi controller function to return a list

--- src/main/java/com/photoproject/service/accounts/nghi/NghiController.java
+++ src/main/java/com/photoproject/service/accounts/nghi/NghiController.java
@@ -1,35 +1,33 @@
 package com.photoproject.service.accounts.nghi;
 
-import com.photoproject.service.accounts.nghi.Nghi;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.concurrent.atomic.AtomicLong;
+
+import java.util.ArrayList;
 
 @RestController
 public class NghiController {
 
-    private final AtomicLong counter = new AtomicLong();
-
     @GetMapping("/nghi")
-    public Nghi nghi(@RequestParam(value = "nname", defaultValue = "0") String nname) {
+    public ArrayList<Integer> nghi(@RequestParam(value = "nname", defaultValue = "0") String nname) {
 
         int n;
-        String result = "";
+        ArrayList<Integer> result = new ArrayList<>();
 
         n = Integer.parseInt(nname);
-        for (int i=100; i<1000; i++) {
-            int tong=0, a, b, c;
+        for (Integer i = 100; i<1000; i++) {
+            int tong, a, b, c;
 
             a = i / 100;
             b = (i % 100) / 10;
             c = i % 10;
             tong = a + b + c;
             if (tong == n) {
-                result = String.format("%s %s", result, n);
+                result.add(i);
             }
         }
 
-        return new Nghi(counter.incrementAndGet(), result);
+        return result;
     }
 }
